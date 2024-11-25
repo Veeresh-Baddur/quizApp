54cc4598a113da4ba83a76374543a34e4d76e357
Veeresh-Baddur <baddurveeresh@gmail.com>
2024-11-25 17:39:07 +0530

    Random Jokes Generator File

---
+++ rondomJoke.js
@@ -0,0 +1,31 @@
+let jokebox = document.querySelector(".jokebox");
+let nextBtn = document.querySelector(".next");
+let viewBtn = document.querySelector(".view")
+
+function view(){
+  fetch('https://official-joke-api.appspot.com/jokes/random')
+  .then(response => response.json())
+  .then(data => {jokebox.innerText = `${data.setup}\n\n"${data.punchline}"`
+  })
+  .catch(error =>{
+    console.error("error in fecthing joke",error)
+  })
+}
+window.onload = view
+
+
+function jokeRequest(){
+fetch('https://official-joke-api.appspot.com/jokes/random')
+  .then(response => response.json())
+  .then(data => {jokebox.innerText = `${data.setup}\n\n"${data.punchline}"`
+  })
+  .catch(error =>{
+    console.error("error in fecthing joke",error)
+  })
+  likeBtn.classList.remove("redBg")
+  likeBtn.classList.add("whiteBg")
+}
+nextBtn.onclick = jokeRequest
+
+let likeBtn = document.querySelector(".like")
+likeBtn.onclick = ()=> likeBtn.classList.contains("whiteBg") ? likeBtn.classList.add("redBg"):likeBtn.classList.add("whiteBg");
